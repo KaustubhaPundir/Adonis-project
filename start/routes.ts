6a46ed974b521cd8692aa0f3f2b1d3f9cc369999
Kaustubha Pundir <kaustubhapundir@gmail.com>
2025-02-05 12:50:43 +0530

    added routes

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,7 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import AuthController from '#controllers/auth_controller'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router.post('/login',[AuthController,'login']);
+router.post('/register',[AuthController,'register']);
